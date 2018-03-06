54230be73e7e4475b18ce54af2c6ac3fc5ce08c8
Fernando Botti <fernando.botti@gmail.com>
2018-03-06 15:25:11 -0300

    Database Config

--- data/configmodel.js
+++ data/configmodel.js
@@ -1,7 +1,27 @@
-var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost/CriptoriumDB');
-mongoose.connect('mongodb://ds157818.mlab.com:57818/heroku_wk6lh0qg', 
-{user: 'heroku_wk6lh0qg', pass: '1qu@nt$01'});
+var mongoose = require('mongoose');    
+
+var uri = 'mongodb://quantbitdbadmin:casa0101@ds157818.mlab.com:57818/heroku_wk6lh0qg';
+
+var options = {
+  "server" : {
+    "socketOptions" : {
+      "keepAlive" : 300000,
+      "connectTimeoutMS" : 30000
+    }
+  },
+  "replset" : {
+    "socketOptions" : {
+      "keepAlive" : 300000,
+      "connectTimeoutMS" : 30000
+    }
+  }
+}
+
+mongoose.connect(uri, options);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 
 var configSchema = new mongoose.Schema({
     LastUpdateCoinAPI: String
