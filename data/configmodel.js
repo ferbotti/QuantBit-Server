f5952526a66e97809ce9fb72d32205cf6714e17b
Fernando Botti <fernando.botti@gmail.com>
2018-03-06 14:56:28 -0300

    Database Config

--- data/configmodel.js
+++ data/configmodel.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/CriptoriumDB');
- 
+//mongoose.connect('mongodb://localhost/CriptoriumDB');
+mongoose.connect('mongodb://ds157818.mlab.com:57818/heroku_wk6lh0qg', 
+{user: 'heroku_wk6lh0qg', pass: '1qu@nt$01'});
+
 var configSchema = new mongoose.Schema({
     LastUpdateCoinAPI: String
 }, { collection: 'configs' }
